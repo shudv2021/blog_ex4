c9a984cf6d356db2018ffcad8359fad38868f4dc
dmitry <shudv@yandex.ru>
2022-08-10 14:16:09 +0300

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    comment.save
+    redirect_to comment.article
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:author, :body, :article_id).to_h.merge(params.permit(:article_id).to_h)
+  end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :email, format:{ :with => /@/}
+  validates :name, presence: true
+  validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+end
