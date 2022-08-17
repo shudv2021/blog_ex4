71ed6f56d99688b8a9c634121ede1c08e71a766f
dmitry <shudv@yandex.ru>
2022-08-17 17:15:58 +0300

    add models rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :articles
-  validates :email, format:{ :with => /@/}
-  validates :name, presence: true
-  validates :email, presence: true
+  
+validates :email, format:{ :with => /@/}
+validates :name, presence: true
+validates :email, presence: true
+ 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ spec/models/users_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+describe User do
+  it { should validate_presence_of :name }
+end
