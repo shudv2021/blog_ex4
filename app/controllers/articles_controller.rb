087c23e6eea4f1f8d502d59a63053be406e4bc59
dmitry <shudv@yandex.ru>
2022-08-17 12:06:56 +0300

    add rcpec to gemfile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
+require 'pry'
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :current_article, only: [:show, :edit, :update]
   
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def new
@@ -23,10 +24,30 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if current_user.id == @article.user_id
+      @article.text = article_params[:text]
+      @article.title = article_params[:title]
+      @article.save
+            
+      redirect_to article_path
+    else
+      flash.alert = "This isn't your article you can't change it"
+      redirect_to articles_path
+    end
+  end
+  
   private
   
+  def current_article
+    @article = Article.find(params[:id])
+  end
+  
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :user_id)
   end
 
 end
