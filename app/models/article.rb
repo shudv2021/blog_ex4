922bf468fe64e236db3f2513dd0e613c75a0e78c
dmitry <shudv@yandex.ru>
2022-08-23 10:29:20 +0300

    add length validation to articlec

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   validates :title, length: { maximum: 140 }
-  
+  validates :text, length: { maximum: 4000 }
+
   has_many :comments
   belongs_to :user
   
