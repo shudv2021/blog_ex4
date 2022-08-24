6a0509636a246930417f04b4ed9d5b07a5a126cd
dmitry <shudv@yandex.ru>
2022-08-24 16:08:45 +0300

    update create message test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 feature 'Contacts Creation' do
   scenario 'allows acers to contact pages' do
     visit '/contacts/new'
-    expect(page).to have_content I18n.t('contacts.contact_us')
+      expect(page).to have_content I18n.t('contacts.contact_us')
   end
+  
+  scenario 'allows guest create test' do
+    visit '/contacts/new'
+      fill_in :email, :with => 'new@mail.ru'
+      fill_in :message, :with => 'new message'
+      click_button 'Send message'
+      expect(page).to have_content 'Thanks for your message!'
+  end 
   
 end
