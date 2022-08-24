38c436d2e5ebc2821879e25a18f14e2cc889b96b
dmitry <shudv@yandex.ru>
2022-08-24 15:48:04 +0300

    update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Contacts Creation' do
   scenario 'allows acers to contact pages' do
     visit '/contacts/new'
-    expect(page).to have_content 'Send message'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
   
 end
